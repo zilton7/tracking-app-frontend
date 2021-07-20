40f56ad0afcb1c87c1b029bddac6ba64d6f9e05b
Zil Norvilis <zilasino27@gmail.com>
2021-07-20 13:07:24 +0200

    Re-style buttom buttons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Measurements from "./components/Measurements";
 function App() {
   return (
     <div className="App">
+      <div className="heading">Tracking App</div>
       <Measurements />
       <Nav />
     </div>
