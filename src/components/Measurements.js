8cd15c1f8c3f6966bb16dead6dc99715690cb031
Zil Norvilis <zilasino27@gmail.com>
2021-07-20 17:18:24 +0200

    Refactor to use MeasurementItem images from api

--- src/components/Measurements.js
+++ src/components/Measurements.js
@@ -1,15 +1,7 @@
 import React, { useEffect } from "react";
-import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import MeasurementItem from "./MeasurementItem";
 import loadMeasurements from "../actions/measurementsAction";
-
-import bicepLeft from "../assets/images/bicep-left.png";
-import bicepRight from "../assets/images/bicep-right.png";
-import waist from "../assets/images/waist.png";
-import hip from "../assets/images/hip.png";
-import legLeft from "../assets/images/leg-left.png";
-import legRight from "../assets/images/leg-right.png";
 
 const Measurements = () => {
   // Fetch measurements data
@@ -18,18 +10,13 @@ const Measurements = () => {
     dispatch(loadMeasurements());
   }, [dispatch]);
 
-  // Put images to array
-  const images = [bicepLeft, bicepRight, waist, hip, legLeft, legRight];
   // Load measurements from state
   let measurements = useSelector((state) => state.measurements.measurements);
 
   return (
     <div className="measurements">
       {measurements.map((measurement) => (
-        <MeasurementItem
-          image={images[measurement.id - 1]}
-          name={measurement.name}
-        />
+        <MeasurementItem image={measurement.image} name={measurement.name} />
       ))}
     </div>
   );
