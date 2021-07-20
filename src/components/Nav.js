1462257dc41ae6f2a4a2adffb795b76a9f08220d
Zil Norvilis <zilasino27@gmail.com>
2021-07-20 09:14:32 +0200

    Add Nav

---
+++ src/components/Nav.js
@@ -0,0 +1,24 @@
+const Nav = () => (
+  <nav>
+    <div class="bottom-buttons">
+      <div class="button-wrapper">
+        <i class="far fa-plus-square"></i>
+        <p>Add measure</p>
+      </div>
+      <div class="button-wrapper">
+        <i class="fas fa-chart-line"></i>
+        <p>Tracker</p>
+      </div>
+      <div class="button-wrapper">
+        <i class="fas fa-chart-pie"></i>
+        <p>Progress</p>
+      </div>
+      <div class="button-wrapper">
+        <i class="fas fa-ellipsis-h"></i>
+        <p>More</p>
+      </div>
+    </div>
+  </nav>
+);
+
+export default Nav;
