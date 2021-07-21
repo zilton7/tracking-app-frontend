26997e5d7b45841358171310601683e775a3ef54
Zil Norvilis <zilasino27@gmail.com>
2021-07-21 11:29:41 +0200

    Implement AddMeasure

--- src/components/AddMeasure.js
+++ src/components/AddMeasure.js
@@ -1,31 +1,71 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 const AddMeasure = () => {
-  const [data, setData] = useState({
-    measurementId: null,
-    measureData: null,
+  const [measurementId, setMeasurementId] = useState({
+    id: 1,
   });
 
+  const [measureData, setMeasureData] = useState({
+    data: 0.01,
+  });
   // Load measurements from state
   let measurements = useSelector((state) => state.measurements.measurements);
 
-  const handleClick = () => {
-    const values = "calculate(buttonName, data)";
-    setData({
-      measurementId: "calculation.total",
-      measureData: "calculation.next",
+  const handleSelectChange = (e) => {
+    setMeasurementId({
+      id: e.target.value,
     });
+  };
+
+  const handleInputChange = (e) => {
+    setMeasureData({
+      data: e.target.value,
+    });
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const payload = {
+      measurement: {
+        id: measurementId.id,
+      },
+      measure: {
+        data: measureData.data,
+      },
+    };
+    axios
+      .post(
+        "https://tracking-app-be-zil.herokuapp.com/api/v1/measurements",
+        payload
+      )
+      .then(() => {
+        document.getElementById("measurement-input").value = "";
+        window.location = `/progress/${measurementId.id}`;
+      });
   };
 
   return (
     <div className="add-measure">
-      <select name="select-measurements" id="select-measurements">
+      <select
+        name="select-measurements"
+        id="select-measurements"
+        onChange={handleSelectChange}
+      >
         {measurements.map((measurement) => (
           <option value={measurement.id}>{measurement.name}</option>
         ))}
       </select>
-      <input placeholder="55.5" />
+      <input
+        id="measurement-input"
+        type="number"
+        step="0.01"
+        min="0"
+        placeholder="0.01"
+        onChange={handleInputChange}
+      />
+      <button onClick={handleClick}>Add</button>
     </div>
   );
 };
